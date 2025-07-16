77946f8213ea43b42c20461f644fcd247943e85b
zack6yuan <zack6yuan@gmail.com>
2025-07-16 17:25:43 -0500

    updates.

--- utils/redis.js
+++ utils/redis.js
@@ -6,6 +6,23 @@ const client = await createClient()
     .on("error", (err) => console.log("Redis Client Error", err))
     .connect();
 
-await client.set("key", "value");
-const value = await client.get("key");
-client.destroy();
+function isAlive() {
+    if (client.isReady) {
+        return true
+    } else {
+        return false
+    }
+}
+
+async function get(key) {
+    const value = await client.get(key)
+    return value
+}
+
+async function set(key, value, duration) {
+    await client.set(key, value, duration)
+}
+
+async function del(key) {
+    await client.del(key)
+}
