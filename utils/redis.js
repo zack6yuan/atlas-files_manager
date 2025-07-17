c940fc1643f76af6c4d1b24fc1230b76783bc398
zack6yuan <zack6yuan@gmail.com>
2025-07-17 14:11:16 -0500

    update doc.

--- utils/redis.js
+++ utils/redis.js
@@ -4,12 +4,14 @@ import { createClient } from "redis";
 
 class RedisClient {
     constructor() {
+    // Creates a redis client
         this.client = createClient()
         .on("error", (err) => console.log("Redis Client Error", err))
         .connect();
     }
 
     isAlive() {
+    // Checks if client was created and ready
         if (this.client.isReady) {
             return true
         } else {
@@ -18,15 +20,19 @@ class RedisClient {
     }
 
     async get(key) {
+    // Returns the redis value stored for a key
         const value = await this.client.get(key)
         return value
     }
 
     async set(key, value, duration) {
+    // Stores values in redis
+    // Duration NEEDS WORK
         await this.client.set(key, value, duration)
     }
 
     async del(key) {
+    // Removes a value in redis for a key
         await this.client.del(key)
     }
 }
