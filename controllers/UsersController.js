5871efc5667e974eadb15a7582edead0f2cc7667
zack6yuan <zack6yuan@gmail.com>
2025-07-21 02:28:53 +0000

    working on the user controller.

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,1 +1,15 @@
 #!/usr/bin/env node
+const { DBClient } = require("db")
+
+const app = express()
+
+class UsersController {
+    postNew(email, password) {
+        if (!email) {
+            console.error(err)
+            return res.status(400).send('Missing email')
+        } else if (!password) {
+            return res.status(400).send("Missing password")
+        }
+    }
+}
