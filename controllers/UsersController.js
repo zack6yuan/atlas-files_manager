fbec8dccf60641e888ee4c348203fc7f51bd3f13
zack6yuan <zack6yuan@gmail.com>
2025-07-21 03:26:58 +0000

    updates.

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,15 +1,28 @@
 #!/usr/bin/env node
-const { DBClient } = require("db")
+const { dbClient } = require("db")
+const { express } = require("express")
 
 const app = express()
 
 class UsersController {
-    postNew(email, password) {
+    async postNew(email, password) {
+        // Check if email is missing
         if (!email) {
-            console.error(err)
             return res.status(400).send('Missing email')
-        } else if (!password) {
+        }
+        // Check if password is missing
+        if (!password) {
             return res.status(400).send("Missing password")
         }
+        // Check if the email is already in the database
+        const emailCheck = await this.dbClient.has(email);
+        if (emailCheck) {
+            return res.status(400).send("Already exist")
+        }
+        // Password needs to be stored after being hashed in SHA1
+        // Endpoint returns the new user with only the email and the id with status code 201
+        // New user must be saved in the collection users
     }
 }
+
+module.exports = UsersController;
