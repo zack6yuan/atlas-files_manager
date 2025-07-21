754206c5594c85034a30ca206185f4472d3701f9
Tyler <tyler.whitchurch@atlasschool.com>
2025-07-21 15:09:54 -0500

    small updates to get api up and running

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const { express } = require("express");
+const express = require('express');
 
 // Module Imports
-const { AppController } = require('../controllers/AppController');
-const { UsersController } = require('../controllers/UsersController');
-const { AuthController } = require('../controllers/AuthController');
+const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
+const AuthController = require('../controllers/AuthController');
 
 const router = express.Router();
 
@@ -13,5 +13,7 @@ router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconect);
-router.get('/users/me', UserController.getMe);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
+
+module.exports = router;
