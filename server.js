a1666ec90ba514e3e47105a0be03efbf0cf5ee42
Tyler <tyler.whitchurch@atlasschool.com>
2025-07-21 15:23:28 -0500

    allow parsing JSON in requests

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+
 const index = require('./routes/index');
 
 app.use('/', index);
